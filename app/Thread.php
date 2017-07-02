64c556194a504f80aa1ec6cc425fe9f19238a491
abhishekush <abhishekush@gmail.com>
2017-07-02 14:01:59 +0530

    Threads module basic implementation with tests

--- app/Thread.php
+++ app/Thread.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     //
+    public function path(){
+        return '/threads/'. $this->id;
+    }
+
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
 }
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+
+class ReadThreadsTest extends TestCase
+{
+    /**
+      @test
+     */
+
+    use DatabaseMigrations;
+
+    public function setUp(){
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+
+    }
+
+    public function a_user_can_view_all_threads()
+    {
+        $response = $this->get('/threads');
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+
+    public function a_user_can_read_single_thread(){
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/'.$this->thread->id);
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+
+    public function a_user_can_see_replies_associated_with_a_thread(){
+        $reply = factory('App\Reply')
+                       ->create(['thread_id'=>$this->thread->id]);
+        $response = $this->get('/threads/'. $this->thread->id);
+        $response->assertSee($reply->body);
+    }
+}
